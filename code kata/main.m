af50c02a5fbb672fefb9865974c385295e4dde97
Jorge Catalan Jr <catalanjrj@gmail.com>
2016-04-25 18:43:53 -0400

    worked on palindrome

--- code kata/main.m
+++ code kata/main.m
@@ -31,11 +31,11 @@ void mutiplesOf3and5();
                 }
             }
             
-            
+            NSLog(@"%d: Multiples of 3 and 5 less than 1000", sum);
         }
     
   
-NSLog(@"%d: Multiples of 3 and 5 less than 1000", sum);
+
 
 }//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.//
 
@@ -44,7 +44,39 @@ NSLog(@"%d: Multiples of 3 and 5 less than 1000", sum);
 
 
 void palendrome(){
+    int result = 0, stringLength = 0;
+    NSString *resultAsString = @"";
+    NSMutableString *reversedString = [[NSMutableString alloc] init];
+    NSMutableString *largestPalindrome = [[NSMutableString alloc] init];
     
+    for (int sequence1 = 100; sequence1 < 1000; sequence1++) {
+        for (int sequence2 = 100; sequence2 < 1000; sequence2++) {
+            
+            [reversedString setString:@""];
+            
+            
+            result = sequence1 * sequence2;
+            
+            
+            resultAsString = [NSString stringWithFormat:@"%d", result];
+            stringLength = (int)[resultAsString length];
+            
+            while (stringLength > 0) {
+                stringLength--;
+                [reversedString appendString:[NSString stringWithFormat:@"%c", (char)[resultAsString characterAtIndex:stringLength]]];
+            }
+            
+            if ([resultAsString isEqualToString:reversedString]) {
+                [largestPalindrome setString:resultAsString];
+            }
+            
+        }
+    }
     
+    NSLog(@"The largest palindrome is: %@", largestPalindrome);
     
 }
+    
+    
+    
+    
